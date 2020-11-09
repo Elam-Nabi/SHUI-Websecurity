f8f153b4152e7e728a447664841e1250726d5c80
Elam Nabi <elam@nabi.nu>
2020-11-09 11:07:48 +0100

    working on the settings page

--- client/src/views/Settings.vue
+++ client/src/views/Settings.vue
@@ -2,10 +2,7 @@
   <div v-show="toggleOpen" class="settings-container">
     <h1>streams</h1>
     <div class="tags-container">
-      <div class="tag-items">
-        <h5 v-for="tag in tags" :key="tag._id">{{ tag.tag }}</h5>
-        <button class="x-button"><span>x</span></button>
-      </div>
+      <h5 v-for="tag in tags" :key="tag._id">{{ tag.tag }} <span>x</span></h5>
     </div>
     <div class="checkbox-container">
       <input
@@ -51,6 +48,7 @@ export default {
 * {
   background: #ef4343;
 }
+
 .settings-container {
   top: 0;
   right: 0;
@@ -67,103 +65,98 @@ export default {
   }
 
   .tags-container {
-    display: flex;
-    margin-left: 10px;
+    display: grid;
+    margin-left: 15px;
+    grid-template-columns: repeat(auto-fit, minmax(130px, 6fr));
 
-    .tag-items {
+    h5 {
+      display: flex;
       margin: 10px;
-      width: 130px;
+      width: 118px;
       opacity: 0.8;
       height: 32px;
       padding: 5px;
       color: #fff;
       font-size: 16px;
+      font-weight: 400;
       border-radius: 4px;
+      font-style: italic;
       background: #d66969;
 
-      h5 {
-        font-weight: 400;
-        font-style: italic;
-        background: #d66969;
-      }
-      .x-button {
-        outline: none;
-        display: flex;
-        margin-top: -5px;
-        margin-left: 93px;
-        position: absolute;
-        border: 1px solid #fff;
-        border-radius: 0 5px 5px 0;
-
-        span {
-          width: 100%;
-          opacity: 0.3;
-          font-size: 30px;
-          margin-top: -10px;
-          background: lightgrey;
-        }
-      }
-    }
-  }
-
-  .checkbox-container {
-    width: 320px;
-    height: 55px;
-    margin: 40px;
-    display: flex;
-    margin-left: 20px;
-    margin-top: 145px;
-    border-radius: 4px;
-    border: 1px solid white;
-
-    input {
-      color: #fff;
-      border: none;
-      outline: none;
-      font-size: 20px;
-      margin-left: 15px;
-      margin-bottom: 5px;
-    }
-
-    h1 {
-      font-size: 23px;
-      margin-top: 10px;
-      font-weight: 300;
-      margin-left: 12px;
-    }
-
-    .addTags-btn {
-      width: 55px;
-      height: 53px;
-      background: #fff;
-      margin-left: 263px;
-      position: absolute;
-      border: 1px solid #fff;
-      border-radius: 0px 4px 4px 0;
-      box-shadow: inset 1px 0px 5px 5px #fff;
-
       span {
-        font-size: 30px;
-        background: #fff;
+        width: 30px;
+        opacity: 0.8;
+        font-size: 24px;
+        margin-top: -5px;
+        margin-left: 86px;
+        position: absolute;
+        font-style: normal;
+        text-align: center;
+        display: inline-block;
+        background: lightgrey;
+        border-radius: 0 4px 4px 0;
       }
     }
   }
+}
 
-  .bottom-btn {
-    width: 320px;
-    height: 50px;
-    margin: 50px;
+.checkbox-container {
+  width: 320px;
+  height: 55px;
+  margin: 40px;
+  display: flex;
+  margin-left: 20px;
+  margin-top: 145px;
+  border-radius: 4px;
+  border: 1px solid white;
+
+  input {
     border: none;
     outline: none;
     color: #fff;
-    cursor: pointer;
-    font-weight: 500;
-    margin-top: -12px;
-    margin-left: 20px;
-    font-size: 1.3rem;
-    border-radius: 4px;
-    position: absolute;
-    background: #082756;
+    font-size: 20px;
+    margin-left: 15px;
+    margin-bottom: 5px;
   }
+
+  h1 {
+    font-size: 23px;
+    margin-top: 10px;
+    font-weight: 300;
+    margin-left: 12px;
+  }
+
+  .addTags-btn {
+    width: 55px;
+    height: 53px;
+    background: #fff;
+    margin-left: 263px;
+    position: absolute;
+    border: 1px solid #fff;
+    border-radius: 0px 4px 4px 0;
+    box-shadow: inset 1px 0px 5px 5px #fff;
+
+    span {
+      font-size: 30px;
+      background: #fff;
+    }
+  }
+}
+
+.bottom-btn {
+  width: 320px;
+  height: 50px;
+  margin: 50px;
+  border: none;
+  outline: none;
+  color: #fff;
+  cursor: pointer;
+  font-weight: 500;
+  margin-top: -12px;
+  margin-left: 20px;
+  font-size: 1.3rem;
+  border-radius: 4px;
+  position: absolute;
+  background: #082756;
 }
 </style>
